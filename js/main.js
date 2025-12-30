6d38ba0dc2e0db38d8598e1f4846640f8586865b
imghoul <nabilchakroune2@gmail.com>
2025-12-30 18:16:12 +0100

    style and boomb

--- js/main.js
+++ js/main.js
@@ -23,27 +23,35 @@ function map() {
     }
 }
 
-    let timer = setInterval(()=>{
-        const time = document.getElementById("time") 
-        time.textContent=time.textContent-10
-        if (time.textContent<=0){
-            clearInterval(timer)
-            gameOver()
-        }            
-    },1000)
-
-    function gameOver(){
-
+let timer = setInterval(() => {
+    const time = document.getElementById("time")
+    time.textContent = time.textContent - 10
+    if (time.textContent <= 0) {
+        clearInterval(timer)
+        gameOver()
     }
+}, 1000)
 
+function gameOver() {
+
+}
+
+function plantBomb() {
+    grid[player.x][player.y].classList.add("bomb")
+}
+const bombe = trottel(plantBomb, 3000)
 document.addEventListener("keydown", (move) => {
+    if (move.key === ' ') {
+        bombe(player.x, player.y)
+        return
+    }
     let x = player.x;
     let y = player.y;
 
-    if (move.key === 'ArrowUp'||move.key === 'w') x--;
-    if (move.key === 'ArrowDown'||move.key === 's') x++;
-    if (move.key === 'ArrowLeft'||move.key === 'a') y--;
-    if (move.key === 'ArrowRight'||move.key === 'd') y++;
+    if (move.key === 'ArrowUp' || move.key === 'w') x--;
+    if (move.key === 'ArrowDown' || move.key === 's') x++;
+    if (move.key === 'ArrowLeft' || move.key === 'a') y--;
+    if (move.key === 'ArrowRight' || move.key === 'd') y++;
     renderPlayer(x, y)
 })
 function renderPlayer(x, y) {
@@ -52,9 +60,25 @@ function renderPlayer(x, y) {
     }
 
     console.log(x, y);
-
+    if (grid[x][y].classList.contains("bomb")) {
+        return
+    }
     grid[player.x][player.y].classList.remove("player")
     grid[x][y].classList.add("player")
     player.x = x
     player.y = y
+}
+function trottel(fn, delay) {
+    let timer;
+    return function (playerX, playerY) {
+        if (!timer) {
+            fn(playerX, playerY)
+            timer = setTimeout(() => {
+                grid[playerX][playerY].classList.remove("bomb")
+                timer = null
+            }, delay)
+
+        }
+    }
+
 }
